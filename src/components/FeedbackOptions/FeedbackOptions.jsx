19f992402bd0119565fee6c048001e4a2cfa0ee5
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-06-04 15:01:52 +0300

    styles

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import css from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ stateGood, stateNeutral, stateBad }) => (
-  <div>
+  <div className={css.feedback_options}>
     <h3>Please leave Feedback</h3>
-    <button type="button" onClick={stateGood}>
-      Good
-    </button>
-    <button type="button" onClick={stateNeutral}>
-      Neutral
-    </button>
-    <button type="button" onClick={stateBad}>
-      Bad
-    </button>
+    <div className={css.feedback_options_list}>
+      <button className={css.button_good} type="button" onClick={stateGood}>
+        Good
+      </button>
+      <button
+        className={css.button_neutral}
+        type="button"
+        onClick={stateNeutral}
+      >
+        Neutral
+      </button>
+      <button className={css.button_bad} type="button" onClick={stateBad}>
+        Bad
+      </button>
+    </div>
   </div>
 );
 
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import NotificationMessage from 'components/NotificationMessage/NotificationMessage';
+import css from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => (
-  <div>
+  <div className={css.statistics}>
     <h3>Statistics</h3>
     {!total ? (
       <NotificationMessage message={'There is no feedback'} />
     ) : (
       <ul>
         <li>
-          <span>Good: {good}</span>
+          <span className={css.statistics_item_good}>Good: {good}</span>
         </li>
         <li>
-          <span>Neutral: {neutral}</span>
+          <span className={css.statistics_item_neutral}>
+            Neutral: {neutral}
+          </span>
         </li>
         <li>
-          <span>Bad: {bad}</span>
+          <span className={css.statistics_item_bad}>Bad: {bad}</span>
         </li>
 
         {total && (
